846f7cf702d6eb2c19f246fb8f3550d79dc02541
Andreas Vishart <45462887+avishart@users.noreply.github.com>
2019-04-25 11:53:21 +0200

    Add files via upload

--- Fit.py
+++ Fit.py
@@ -8,7 +8,7 @@ import sys
 
 #------------------------Parameters----------------------
 #Seperation symbol
-sep=","
+sep=";"
 #The name of saved plot
 title="EC_DBA.png"
 #X-axis title 
@@ -17,10 +17,17 @@ x_label="Distance / [Angstrom]"
 y_label="EC / [mHartree]"
 #List of colors for plotting
 color=["red","blue","green","orange","pink","purple","black","yellow"]
+#List of markers for plotting
+markers=["o","s","o","o","o","o","o","o"]
 #Want to Fit data (y/n)
 fit_data="n"
+#Function to fit to
+func_fit="coeff[1]*np.exp(coeff[0]*x)"
+#X label is a string (y/n)
+x_str="y"
 
 #------------------------Functions-----------------------
+#Remove all none values
 def rm_none_values(x_list,y_list):
     x_list_new=[]
     y_list_new=[]
@@ -32,16 +39,19 @@ def rm_none_values(x_list,y_list):
             x_list_new.append(x_list[i])
     return x_list_new,y_list_new
 
+#Fit data to an exponential function
 def fit_exp(x_list,y_list):
     y_list_new=[np.log(y) for y in y_list]
     coef=np.polyfit(x_list,y_list_new,1)
     coef[1]=np.exp(coef[1])
     return coef
 
+#Calculate the values of an function
 def y_func(x_list,funct,coeff):
     y_list=[eval(funct) for x in x_list]
     return y_list
 
+#Calculate the R2 value of the fit
 def R_squared(y_list,y_ref):
     len_y_ref=len(y_ref)
     mean_ref=sum(y_ref)/len_y_ref
@@ -53,39 +63,57 @@ def R_squared(y_list,y_ref):
 
 
 #------------------------Program-------------------------
+#Load the .csv
 with open(sys.argv[1]) as thefile:
     content=thefile.readlines()
 
-label=list(filter(None,content[0].split(sep)))[1:]
+#Get the labels of y values and remove them without
+label=list(filter(None,content[0].split(sep)))
 content=content[1:]
 x_list=[]
 len_list=len(list(filter(None,content[0].split(sep))))
 y_list=[[] for i in range(len_list-1)]
+#Get the data to x and y values
 for line in content:
     newline=list(filter(None,line.split(sep)))
-    x_list.append(float(newline[0]))
+    x_list.append(newline[0])
     for i in range(1,len_list):
         try:
             y_list[i-1].append(float(newline[i]))
         except:
             y_list[i-1].append(None)
 
-x_conti=np.linspace(x_list[0],x_list[-1],(x_list[-1]-x_list[0])/0.1)
+#If strings are used for x values then make a new x list
+if x_str.lower()=="y" or x_str.lower()=="yes":
+    x_num_list=[i+1 for i in range(len(x_list))]
+    plt.xticks(x_num_list,x_list,rotation=-60)
+#If values are used as x values then make them floats
+else:
+    x_list=map(float,x_list)
+    x_conti=np.linspace(x_list[0],x_list[-1],(x_list[-1]-x_list[0])/0.1)
 
+#Plot data
 for i in range(len_list-1):
-    x_list_new,y_list_new=rm_none_values(x_list,y_list[i])
-    plt.plot(x_list_new,y_list_new,color=color[i],marker="o",linestyle="",label=label[i])
-    if fit_data.lower()=="y" or fit_data.lower()=="yes":
-        coeff=fit_exp(x_list_new,y_list_new)
-        R2=R_squared(y_func(x_list_new,"coeff[1]*np.exp(coeff[0]*x)",coeff),y_list_new)
-        plt.plot(x_conti,y_func(x_conti,"coeff[1]*np.exp(coeff[0]*x)",coeff),color=color[i],linestyle="-",label=str(round(coeff[1],1))+"*exp("+str(round(coeff[0],3))+"*x),$R^2$="+str(round(R2,3)))
+    if x_str.lower()=="y" or x_str.lower()=="yes":
+        x_list_new,y_list_new=rm_none_values(x_num_list,y_list[i])
+        plt.plot(x_list_new,y_list_new,color=color[i],marker=markers[i],linestyle="",label=label[i])
+    else:
+        x_list_new,y_list_new=rm_none_values(x_list,y_list[i])
+        plt.plot(x_list_new,y_list_new,color=color[i],marker=markers[i],linestyle="",label=label[i])
+        #Fit only if x data are values and required
+        if fit_data.lower()=="y" or fit_data.lower()=="yes":
+            coeff=fit_exp(x_list_new,y_list_new)
+            R2=R_squared(y_func(x_list_new,func_fit,coeff),y_list_new)
+            plt.plot(x_conti,y_func(x_conti,func_fit,coeff),color=color[i],linestyle="-",label=str(round(coeff[1],1))+"*exp("+str(round(coeff[0],3))+"*x),$R^2$="+str(round(R2,3)))
 
-plt.legend()
+#Show the plot
+plt.legend(loc=0)
 plt.xlabel(x_label)
 plt.ylabel(y_label)
+plt.tight_layout()
 plt.savefig(title)
 plt.show()
-
+plt.close()
 
 
 
